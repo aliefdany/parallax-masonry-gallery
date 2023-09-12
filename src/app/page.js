13e778e891a1eb17c97c658267e24b099bc473e7
aliefdany <works@alief.tech>
2023-09-12 13:55:14 +0700

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,219 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image";
+
+import { Diplomata, Aboreto } from "next/font/google";
+
+import masonry1 from "../../public/masonry-1.jpg";
+import masonry2 from "../../public/masonry-2.jpg";
+import masonry3 from "../../public/masonry-3.jpg";
+import masonry4 from "../../public/masonry-4.jpg";
+import masonry5 from "../../public/masonry-5.jpg";
+import masonry6 from "../../public/masonry-6.jpg";
+import masonry7 from "../../public/masonry-7.jpg";
+import masonry8 from "../../public/masonry-8.jpg";
+import masonry9 from "../../public/masonry-9.jpg";
+import masonry10 from "../../public/masonry-10.jpg";
+import masonry11 from "../../public/masonry-11.jpg";
+
+import { useRef, useLayoutEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+const diplomata = Diplomata({ subsets: ["latin"], weight: "400" });
+const aboreto = Aboreto({ subsets: ["latin"], weight: "400" });
 
 export default function Home() {
+  const gridRef = useRef();
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.registerPlugin(ScrollTrigger);
+
+      gsap.to(".masonry-1 img.potrait", {
+        yPercent: -50,
+        scrollTrigger: {
+          scrub: 0.2,
+          // markers: true,
+          start: "top 10px",
+          end: "bottom 710px",
+        },
+      });
+
+      gsap.to(".masonry-1 img.landscape", {
+        yPercent: -50,
+        scrollTrigger: {
+          scrub: 0.2,
+          // markers: true,
+          start: "top 10px",
+          end: "bottom 310px",
+        },
+      });
+
+      gsap.to("h1", {
+        y: -100,
+        scrollTrigger: {
+          scrub: 0.2,
+          start: "top 400px",
+          end: "bottom top",
+          // markers: true,
+        },
+      });
+
+      const tl = gsap.timeline();
+
+      tl.from(".masonry-1 > div", {
+        y: 150,
+        stagger: 0.1,
+        autoAlpha: 0,
+      }).from(
+        "h1",
+        {
+          y: 50,
+          stagger: 0.1,
+          autoAlpha: 0,
+        },
+        "<+0.5"
+      );
+    }, gridRef);
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="p-10">
+      <div
+        ref={gridRef}
+        className="masonry-1 hidden lg:grid w-full sm:w-11/12 xl:w-3/5 mx-auto lg:grid-cols-2 lg:grid-rows-9 gap-10 relative"
+      >
+        <h1
+          className={
+            "absolute invisible bg-white text-7xl text-black font-bold z-20 top-[420px] -left-10" +
+            " " +
+            aboreto.className
+          }
+        >
+          Capture <br /> your <br /> moments
+        </h1>
+
+        <h1
+          className={
+            "absolute invisible bg-black text-7xl text-white font-bold z-20 top-[820px] -right-20" +
+            " " +
+            aboreto.className
+          }
+        >
+          Define <br /> your <br /> reality
+        </h1>
+
+        <h1
+          className={
+            "absolute invisible bg-white text-7xl text-black font-bold z-20 top-[1850px] -left-10" +
+            " " +
+            aboreto.className
+          }
+        >
+          Embrace <br /> the <br /> nature
+        </h1>
+
+        <h1
+          className={
+            "absolute invisible bg-black text-7xl text-white font-bold z-20 top-[2800px] -right-10" +
+            " " +
+            aboreto.className
+          }
+        >
+          Explore <br /> the <br /> beauty
+        </h1>
+        <div className="overflow-hidden invisible row-span-2 h-[700px] relative">
+          <Image
+            alt="picture of a woman"
+            fill={true}
+            src={masonry1.src}
+            className="brightness-100 object-cover scale-150 origin-top potrait"
+          />
+        </div>
+        <div className="overflow-hidden invisible h-[300px] relative">
+          <Image
+            alt="picture of a plant"
+            fill={true}
+            src={masonry2.src}
+            className="brightness-100 object-cover scale-150 origin-top landscape"
+          />
+        </div>
+        <div className="overflow-hidden invisible row-span-2 h-[700px] relative">
+          <Image
+            alt="picture of a san fransico bridge"
+            fill={true}
+            src={masonry3.src}
+            className="brightness-100 object-cover scale-150 origin-top potrait"
+          />
+        </div>
+        <div className="overflow-hidden invisible h-[300px] relative">
+          <Image
+            alt="picture of crow on a fence"
+            fill={true}
+            src={masonry4.src}
+            className="brightness-100 object-cover scale-150 origin-top landscape"
+          />
+        </div>
+        <div className="overflow-hidden invisible bg-gray-300 h-[300px] relative">
+          <Image
+            alt="picture of an electric tower"
+            fill={true}
+            src={masonry5.src}
+            className="brightness-100 object-cover object-cover scale-150 origin-top landscape"
+          />
+        </div>
+        <div className="overflow-hidden invisible bg-gray-300 row-span-2 h-[700px] relative">
+          <Image
+            alt="picture of a building with stairs"
+            fill={true}
+            src={masonry6.src}
+            className="brightness-100 object-cover scale-150 origin-top potrait"
+          />
+        </div>
+        <div className="overflow-hidden invisible bg-gray-300 row-span-2 h-[700px] relative">
+          <Image
+            alt="picture of a horse"
+            fill={true}
+            src={masonry8.src}
+            className="brightness-100 object-cover scale-150 origin-top potrait"
+          />
+        </div>
+        <div className="overflow-hidden invisible bg-gray-300 h-[300px] relative">
+          <Image
+            alt="picture of woman in black and white outfit"
+            fill={true}
+            src={masonry7.src}
+            className="brightness-100 object-cover object-cover scale-150 origin-top landscape"
+          />
+        </div>
+        <div className="overflow-hidden invisible bg-gray-300 h-[300px] relative">
+          <Image
+            alt="picture of boat with colony of birds"
+            fill={true}
+            src={masonry9.src}
+            className="brightness-100 object-cover object-cover scale-150 origin-top landscape"
+          />
+        </div>
+        <div className="overflow-hidden invisible bg-gray-300 row-span-3 relative">
+          <Image
+            alt="picture of woman at the beach"
+            fill={true}
+            src={masonry11.src}
+            className="brightness-100 object-cover scale-150 origin-top potrait"
+          />
+        </div>
+        <div className="overflow-hidden invisible bg-gray-300 row-span-2 h-[700px] relative">
+          <Image
+            alt="picture of road sign in the city"
+            fill={true}
+            src={masonry10.src}
+            className="brightness-100 object-cover scale-150 origin-top potrait"
+          />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
